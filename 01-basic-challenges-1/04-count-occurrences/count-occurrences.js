9912a4764244a70b578c8d7dc1da151b3b99d40b
Marshall Brown <marshallendel@gmail.com>
2023-09-28 11:21:43 -0400

    complete 04 occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,20 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+    return string.split("").filter(letter => letter === char).length
+
+    // let occurrences = 0;
+    // for(let i = 0; i < string.length; i++) {
+    //     if(string[i] === char) {
+    //         occurrences++
+    //     }
+    // }
+    // return occurrences
+
+    // for (const letter of string) {
+    //     if(letter === char) {
+    //         occurrences++
+    //     }
+    // }
+    // return occurrences
+}
 
 module.exports = countOccurrences;
