6c6938a5434a47dace9f8c6b3d16b5db01a16a77
Marshall Brown <marshallendel@gmail.com>
2023-09-28 12:37:29 -0400

    complete find palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    let filteredString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    if(filteredString.length === 0 || filteredString.length === 1) return true
+    if(filteredString.length === 2) return filteredString[0] === filteredString[1]
+    if(filteredString[0] !== filteredString.at(-1)) return false
+    return isPalindrome(filteredString.slice(1, -1))
+    // return filteredString === filteredString.split("").reverse().join("")
+}
 
 module.exports = isPalindrome;
