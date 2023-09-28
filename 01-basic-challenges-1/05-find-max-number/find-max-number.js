7f91719b8a0e94fded4bd6803d123322dc8054c7
Marshall Brown <marshallendel@gmail.com>
2023-09-28 11:34:25 -0400

    complete max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,13 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    // let max = null;
+    // for(let i = 0; i < arr.length; i++) {
+    //     if(arr[i] > max) {
+    //         max = arr[i]
+    //     }
+    // }
+    // return max
+
+    return Math.max(...arr)
+}
 
 module.exports = findMaxNumber;
